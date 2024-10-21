c3cece2594f8d3744ce5fd8230475756316acca1
ronalynbaniagas <baniagasronalyn@gmail.com>
2024-10-21 10:51:05 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+const favSingers = ["Bruno Mars", "Justin Bieber", "Ariana Grande"];
+
+console.log(favSingers[2]);
+
+const favNumbers = [17, 8, 14, 19];
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
